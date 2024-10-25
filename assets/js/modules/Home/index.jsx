e6830792c798ee0405db23fedda98291cfe2d366
Drakos <orlando.andaur.c@gmail.com>
2024-10-25 13:15:29 -0300

    advances in menu

--- assets/js/modules/Home/index.jsx
+++ assets/js/modules/Home/index.jsx
@@ -29,7 +29,7 @@ const HomeApp = () => {
     <div>
       <Carousel images={images}/>
       <Menu onSelect={actions.onSelectOptionMenu}/>
-      <div className='card__section'>
+      <div className="card__section">
         <Card text="OFERTA 1" backgroundImg={`/static/${VERSION}/images/cards/item-1.jpg`} />
         <Card text="OFERTA 2" backgroundImg={`/static/${VERSION}/images/cards/item-1.jpg`} />
         <Card text="OFERTA 3" backgroundImg={`/static/${VERSION}/images/cards/item-1.jpg`} />
